01194e835b9dddaf02df9fbd803572ef150078f7
Noman <pxycknomdictator@gmail.com>
2024-12-09 03:15:51 +0500

    analyze go grouping

--- grouped_declaration/group.go
+++ grouped_declaration/group.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
+var (
+	isLoggedIn = true
+	isAuthentic = false
+	isAdmin = false
+)
+
 func main(){
 	username, age := "Noman", 69
 	fmt.Println(username, age)
+
+	isAdmin = true
+
+	fmt.Println(isAuthentic, isLoggedIn, isAdmin)
 
 }
